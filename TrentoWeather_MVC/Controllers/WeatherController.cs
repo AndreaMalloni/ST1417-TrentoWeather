5a053faff8c9b9cf54c04ce8cc6f216799ae6e16
Daniele Monaldi <daniele-monaldi@hotmail.it>
2023-12-27 11:56:24 +0100

    Added SOAP service reference

---
+++ TrentoWeather_MVC/Controllers/WeatherController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceReference1;
+using TrentoWeather_MVC.ViewModels;
+
+namespace TrentoWeather_MVC.Controllers
+{
+    public class WeatherController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            ISoapService soapServiceChannel = new SoapServiceClient(SoapServiceClient.EndpointConfiguration.BasicHttpBinding_ISoapService_soap);
+            var sunResponse = soapServiceChannel.GetWeatherAsync(null).Result;
+
+            
+            return View();
+        }
+    }
+}
---
+++ TrentoWeather_MVC/ViewModels/WeatherIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿using static TrentoWeather_SOAP.Models.models;
+
+namespace TrentoWeather_MVC.ViewModels
+{
+    public class WeatherIndexViewModel
+    {
+        public Giorni[] listaGiorni { get; set; }
+    }
+}
